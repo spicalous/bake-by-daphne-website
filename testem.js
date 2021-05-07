3bef9148b605d0581c9887cb59efc39db0acebc5
spicalous <benlertlumprasertkul@gmail.com>
2021-05-07 17:02:19 +0100

    Initial template

--- testem.js
+++ testem.js
@@ -3,7 +3,7 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
+  launch_in_ci: ['Firefox'],
   launch_in_dev: ['Chrome'],
   browser_start_timeout: 120,
   browser_args: {
